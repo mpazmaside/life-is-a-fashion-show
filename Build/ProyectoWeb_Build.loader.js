7decb1d036149057960a8f0db029d39d218a4235
mpazmaside <mpazmaside@gmail.com>
2026-06-18 17:10:54 -0300

    nueversionnew

--- Build/ProyectoWeb_Build.loader.js
+++ Build/ProyectoWeb_Build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58207193,
+    wasmFileSize: 58207885,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
